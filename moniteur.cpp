5c539f8bf8a47ae884b96fa71688add469e67a98
Cecilia <cecilia.lejeu@gmail.com>
2013-01-22 12:04:09 +0100

    Player fully integrated in the main window

--- moniteur.cpp
+++ moniteur.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <QFile>
 #include <opencv2/highgui/highgui.hpp>
 #include "moniteur.h"
 #include "player/Player.h"
@@ -14,8 +15,14 @@ Moniteur::Moniteur(QWidget *parent):QWidget(parent)
 
     QHBoxLayout* layout = new QHBoxLayout(this);
     layout->addWidget(player);
+    player->resize(200,200);
     player->show();
 
+    /*Setting CSS file*/
+    QFile file("../player/playerStyleSheet.css");
+    file.open(QFile::ReadOnly);
+    QString playerStyleSheet = QLatin1String(file.readAll());
+    player->setStyleSheet(playerStyleSheet);
 
     labMoniteur = new QLabel("Moniteur",this);
     labMoniteur->setFont(Mfont);
