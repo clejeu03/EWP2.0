839f281e6e4cfbf76e68f09a511f3e5ad4144680
dam <damaris.ankou@gmx.com>
2013-06-18 17:49:21 +0200

    Revert "commit before branch change"
    This reverts commit 0cbb8419dffb4440a39ad2097a137911045dc7d0.

--- main.cpp
+++ main.cpp
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
     qDebug() << "list size : " << timeline->getVideoList();
     qDebug() << "keys : " << timeline->getVideoList().uniqueKeys();
 
-   // timeline.addVideo(video1);
-   // timeline.addVideo(video2);
+    //timeline.addVideo(video1);
+    //timeline.addVideo(video2);
 
     qDebug() << "__________ADDING__________";
     qDebug() << "list size : " << timeline->getListSize();
@@ -39,13 +39,13 @@ int main(int argc, char *argv[])
     qDebug() << "adding 2 videos : " << timeline->getVideoList().value(1)->getCompleteName();
 
     qDebug() << "__________REMOVING VIDEO1__________";
-  //  timeline->removeVideo(0);
+    timeline->removeVideo(0);
     qDebug() << "list size : " << timeline->getListSize();
     qDebug() << "keys : " << timeline->getVideoList().uniqueKeys();
     qDebug() << "list(0) : " << timeline->getVideoList().value(0)->getCompleteName();
     //qDebug() << "list(1) : " << timeline.getVideoList().value(1)->getCompleteName();
 
-  //  timeline->addVideo(video1);
+    timeline->addVideo(video1);
 
      qDebug() << "__________ADDING VIDEO2__________";
      qDebug() << "list size : " << timeline->getListSize();
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
      qDebug() << "list(0) : " << timeline->getVideoList().value(0)->getCompleteName();
      qDebug() << "list(1) : " << timeline->getVideoList().value(1)->getCompleteName();
 
-    //timeline->switchVideo(video1, video2);
+    timeline->switchVideo(video1, video2);
 
     qDebug() << "__________MOVING VIDEOS__________";
     qDebug() << "list size : " << timeline->getListSize();
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
     qDebug() << " video 1 : " << video1->getReverse();
     qDebug() << " video 2 : " << video2->getReverse();
 
- //   timeline->clear();
+    timeline->clear();
     qDebug() << "__________CLEAR__________";
     qDebug() << "list size : " << timeline->getListSize();
     qDebug() << "keys : " << timeline->getVideoList().uniqueKeys();
